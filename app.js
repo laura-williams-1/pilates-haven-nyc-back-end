fb93f7eeb9926d8637eaa1a1b0c82f9bdb55279d
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-20 12:01:25 -0400

    studios controller added to app.js file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const studiosController = require("./Controllers/productsController");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/studios", studios);
 
 app.get("/", (req, res) => {
   app.status(200).response("Welcome to the NYC Pilates Haven");
